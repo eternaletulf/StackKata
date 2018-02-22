bae98f560eddb484eeea0ab957482215694ac657
Mauricio Sepúlveda <msepulveda@nisum.com>
2018-02-22 02:27:00 -0300

    can create a stack

--- src/test/java/StackTest.java
+++ src/test/java/StackTest.java
@@ -1,5 +1,13 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class StackTest {
 
+    @Test
+    public void shouldBeAbleToCreateAStack(){
+        Stack stack = new Stack();
+
+        assertNotNull(stack);
+    }
 }
